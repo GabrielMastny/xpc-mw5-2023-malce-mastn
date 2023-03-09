fd44863834c3865d7584fc8a894ee7f545e2c95b
Gabriel Mastny <gmastny@bjsw.cz>
2023-03-09 18:35:29 +0100

    Added Commodity controller

---
+++ WebAPI/Controllers/CommodityController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommodityController : ControllerBase
+    {
+        private readonly ILogger<CommodityController> _logger;
+
+        public CommodityController(ILogger<CommodityController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            var rng = new Random();
+            return new List<object>();
+        }
+    }
+}
