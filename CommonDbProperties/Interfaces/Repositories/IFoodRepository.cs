600d23bd26817f6f67d1a1cdf91fdad4ca438a15
Gabriel Mastny <gmastny@bjsw.cz>
2023-04-05 00:57:02 +0200

    removed template repo

--- CommonDbProperties/Interfaces/Repositories/IFoodRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using CommonDbProperties.Interfaces.Entities;
-
-namespace CommonDbProperties.Interfaces.Repositories;
-
-public interface IFoodRepository<TEntity> : IRepository<TEntity> where TEntity : class, IWithId
-{
-    
-}
