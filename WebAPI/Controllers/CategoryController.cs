b93fff45f14142398a8808a25ebd59106e9b6c23
Gabriel Mastny <gmastny@bjsw.cz>
2023-04-05 00:58:25 +0200

    finished category controller

--- WebAPI/Controllers/CategoryController.cs
+++ WebAPI/Controllers/CategoryController.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using App.DAL.Entities;
 using AutoMapper;
 using CommonDbProperties.Interfaces.Repositories;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebAPI.Dtos;
@@ -24,20 +27,58 @@ public class CategoryController
     }
 
     [HttpGet]
-    public IEnumerable<CategoryEntity> Get()
+    public IEnumerable<CategoryDTO> Get()
     {
-        return new List<CategoryEntity>();
+        return _repo.Get().Select(x => _mapper.Map<CategoryDTO>(x));
     }
     
     [HttpPost(Name = nameof(AddCategory))]
     public ActionResult<string> AddCategory(ApiVersion version, [FromBody] CategoryCreateDTO categoryCreateDto)
     {
-        var ent = _mapper.Map<CategoryEntity>(categoryCreateDto);
-        var newG = _repo.Create(ent);
+        var newG = _repo.Create(_mapper.Map<CategoryEntity>(categoryCreateDto));
+        
 #if DEBUG
-        return newG.ToString();
+        return newG.ToString(); 
 #else
-            return Ok();
+        return (Guid.Empty == newG) ? new BadRequestResult() : new OkResult();
 #endif
+    }
+    
+    [HttpGet]
+    [Route("{id:Guid}", Name = nameof(GetSingleCategory))]
+    public ActionResult<CategoryDTO> GetSingleCategory(ApiVersion version, Guid id)
+    {
+       var cat =  _repo.GetById(id);
+       
+       if (Guid.Empty == cat.Id)
+       {
+           return new NotFoundResult();
+       }
+       
+       return _mapper.Map<CategoryDTO>(cat);
+    }
+
+    [HttpPut]
+    [Route("{id:Guid}", Name = nameof(UpdateCategory))]
+    public ActionResult<CategoryDTO> UpdateCategory(ApiVersion version, Guid id, [FromBody] CategoryDTO categoryDto)
+    {
+        if (categoryDto == null)
+            return new BadRequestResult();
+        
+        var cat = _repo.Update(_mapper.Map<CategoryEntity>(categoryDto));
+
+        if (Guid.Empty == cat.Id)
+            return new NotFoundResult();
+
+        return _mapper.Map<CategoryDTO>(cat);
+    }
+
+    [HttpDelete]
+    [Route("{id:Guid}", Name = nameof(RemoveCategory))]
+    public ActionResult RemoveCategory(Guid id)
+    {
+        _repo.Delete(id);
+
+        return new OkResult();
     }
 }
