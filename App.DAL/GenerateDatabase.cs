fde2eeb2aa417a850ac1a40866dec19b6e0785d4
Gabriel Mastny <gmastny@bjsw.cz>
2023-04-11 00:06:42 +0200

    little tweaks to bogus

--- App.DAL/GenerateDatabase.cs
+++ App.DAL/GenerateDatabase.cs
@@ -5,19 +5,23 @@ namespace App.DAL;
 
 public class GenerateDatabase : IDisposable
 {
-
+    private List<ManufacturerEntity> manufacturers = new List<ManufacturerEntity>();
+    private List<CategoryEntity> categories = new List<CategoryEntity>();
+    private Random rnd = new Random();
+    private const int RATIO = 4;
     public ICollection<CommodityEntity> init(int number)
     {
         var faker = new Faker<CommodityEntity>()
             .UseSeed(100)
+            .RuleFor(c => c.Id, f => Guid.NewGuid())
             .RuleFor(c => c.Name, f => f.Name.FirstName())
             .RuleFor(c => c.Image, f => f.Random.String(1))
             .RuleFor(c => c.Description, f => f.Lorem.Sentence())
             .RuleFor(c => c.Price, f => f.Random.Number(1, 1000))
             .RuleFor(c => c.Weight, f => f.Random.Number(1, 100))
             .RuleFor(c => c.NumberOfPiecesInStock, f => f.Random.Int(0, 2000))
-            .RuleFor(c => c.Manufacturer, f => GenerateFakeManufacturer())
-            .RuleFor(c => c.Category, f => GenerateFakeCategory());
+            .RuleFor(c => c.Manufacturer, f => GenerateFakeManufacturer(number))
+            .RuleFor(c => c.Category, f => GenerateFakeCategory(number));
 
         return faker.Generate(number);
     }
@@ -29,24 +33,53 @@ public class GenerateDatabase : IDisposable
             .RuleFor(r => r.Stars, f => f.Random.Int(0, 5))
             .RuleFor(r => r.Description, f => f.Lorem.Sentence())
             .RuleFor(r => r.Title, f => f.Name.FirstName())
-            .Generate(number);
+            .Generate( rnd.Next(number/ RATIO));
     }
 
-    private Faker<CategoryEntity> GenerateFakeCategory()
+    private CategoryEntity GenerateFakeCategory(int count)
     {
-        return new Faker<CategoryEntity>()
-            .UseSeed(98)
-            .RuleFor(c => c.Name, f => f.Name.FirstName());
+        if (categories.Count < count / RATIO)
+        {
+            var cat =  new Faker<CategoryEntity>()
+                .UseSeed(98)
+                .RuleFor(c => c.Id, f => Guid.NewGuid())
+                .RuleFor(c => c.Name, f => f.Name.FirstName())
+                .RuleFor(c => c.Description, f => f.Lorem.Sentence())
+                .RuleFor(c => c.Image, f => f.Image.Random.String())
+                .Generate();
+            
+            categories.Add(cat);
+            return cat;
+        }
+        else
+        {
+            return categories.ElementAt(rnd.Next(categories.Count));
+        }
+        
+        
+
     }
 
-    private Faker<ManufacturerEntity> GenerateFakeManufacturer()
+    private ManufacturerEntity GenerateFakeManufacturer(int count)
     {
-        return new Faker<ManufacturerEntity>()
-            .UseSeed(97)
-            .RuleFor(m => m.Name, f => f.Name.FirstName())
-            .RuleFor(m => m.Description, f => f.Lorem.Sentence())
-            .RuleFor(m => m.Image, f => f.Random.String(1))
-            .RuleFor(m => m.CountryOfOrigin, f => f.Name.FirstName());
+        if (manufacturers.Count() < count / RATIO)
+        {
+           var man = new Faker<ManufacturerEntity>()
+                .UseSeed(97)
+                .RuleFor(m => m.Id, f => Guid.NewGuid())
+                .RuleFor(m => m.Name, f => f.Name.FirstName())
+                .RuleFor(m => m.Description, f => f.Lorem.Sentence())
+                .RuleFor(m => m.Image, f => f.Random.String(1))
+                .RuleFor(m => m.CountryOfOrigin, f => f.Name.FirstName())
+                .Generate();
+           manufacturers.Add(man);
+           return man;
+        }
+        else
+        {
+            return manufacturers.ElementAt(rnd.Next(manufacturers.Count));
+        }
+        
     }
 
     public void Dispose()
