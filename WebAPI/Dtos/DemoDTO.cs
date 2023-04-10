5379192f054fba810f0ad3fb6cd1d2a699e4fe9b
Gabriel Mastny <gmastny@bjsw.cz>
2023-04-11 00:06:59 +0200

    added functionality for demo

---
+++ WebAPI/Dtos/DemoDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace WebAPI.Dtos;
+
+public class DemoDTO
+{
+    public IEnumerable<CategoryDTO> Categories { get; set; }
+
+    public IEnumerable<ManufacturerBriefDTO> Manufacturers { get; set; }
+
+    public IEnumerable<CommodityDto> Commodities { get; set; }
+}
