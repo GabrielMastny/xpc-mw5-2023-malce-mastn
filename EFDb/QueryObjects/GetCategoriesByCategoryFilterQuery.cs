cfbe8a3fd154c24ff73f5a0fd7e1c0ea70d99e1e
xmalce00 <60735480+xmalce00@users.noreply.github.com>
2023-04-10 18:53:15 +0200

    Update queries

--- EFDb/QueryObjects/GetCategoriesByCategoryFilterQuery.cs
+++ EFDb/QueryObjects/GetCategoriesByCategoryFilterQuery.cs
@@ -20,8 +20,6 @@ public class GetCategoriesByCategoryFilterQuery : IQuery<CategoryEntity, Categor
     
     public IEnumerable<CategoryEntity> Execute(CategoryFilter filter)
     {
-        //return _db.Categories.Select(c => c.Name == filter.Name).SingleOrDefault();
-        throw new NotImplementedException();
-
+        return _db.Categories.Select(x => _mapper.Map<CategoryEntity>(x)).Where(x => x.Name == filter.Name);;
     }
 }
--- EFDb/QueryObjects/GetReviewsByReviewFilterQuery.cs
+++ EFDb/QueryObjects/GetReviewsByReviewFilterQuery.cs
@@ -1,13 +1,24 @@
 ﻿using App.DAL.Entities;
+using AutoMapper;
 using CommonDbProperties.Interfaces.Filters;
 using CommonDbProperties.Interfaces.QueryObjects;
+using EFDb.Context;
 
 namespace EFDb.QueryObjects;
 
 public class GetReviewsByReviewFilterQuery : IQuery<ReviewEntity, ReviewFilter>
 {
+    
+    private readonly EshopContext _db;
+    private readonly IMapper _mapper;
+    
+    public GetReviewsByReviewFilterQuery(EshopContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
     public IEnumerable<ReviewEntity> Execute(ReviewFilter filter)
     {
-        throw new NotImplementedException();
+        return _db.Reviews.Select(x => _mapper.Map<ReviewEntity>(x)).Where(x => x.Stars == filter.Stars);
     }
 }
