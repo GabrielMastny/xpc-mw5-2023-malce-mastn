b94a4ca78150caa375651ef7c069c5673be1fef3
xmalce00 <60735480+xmalce00@users.noreply.github.com>
2023-04-10 19:00:26 +0200

    Update queries

--- EFDb/QueryObjects/GetManufacturersByManufacturerDataFilterQuery.cs
+++ EFDb/QueryObjects/GetManufacturersByManufacturerDataFilterQuery.cs
@@ -1,13 +1,27 @@
 ﻿using App.DAL.Entities;
 using App.DAL.QueryObjects.Filters;
+using AutoMapper;
 using CommonDbProperties.Interfaces.QueryObjects;
+using EFDb.Context;
 
 namespace EFDb.QueryObjects;
 
 public class GetManufacturersByManufacturerDataFilterQuery : IQuery<ManufacturerEntity, ManufacturerDataFilter>
 {
+    
+    private readonly EshopContext _db;
+    private readonly IMapper _mapper;
+    
+    public GetManufacturersByManufacturerDataFilterQuery(EshopContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
     public IEnumerable<ManufacturerEntity> Execute(ManufacturerDataFilter filter)
     {
-        throw new NotImplementedException();
+        IEnumerable<ManufacturerEntity> list = _db.Comodities.Select(m => _mapper.Map<ManufacturerEntity>(m));
+        if (filter.Name != null) list = list.Where(m => m.Name.Contains(filter.Name));
+        if (filter.CountryOfOrigin != null) list = list.Where(m => m.CountryOfOrigin == filter.CountryOfOrigin);
+        return list;
     }
 }
--- EFDb/QueryObjects/GetReviewsByReviewFilterQuery.cs
+++ EFDb/QueryObjects/GetReviewsByReviewFilterQuery.cs
@@ -1,13 +1,23 @@
 ﻿using App.DAL.Entities;
+using AutoMapper;
 using CommonDbProperties.Interfaces.Filters;
 using CommonDbProperties.Interfaces.QueryObjects;
+using EFDb.Context;
 
 namespace EFDb.QueryObjects;
 
 public class GetReviewsByReviewFilterQuery : IQuery<ReviewEntity, ReviewFilter>
 {
+    private readonly EshopContext _db;
+    private readonly IMapper _mapper;
+    
+    public GetReviewsByReviewFilterQuery(EshopContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
     public IEnumerable<ReviewEntity> Execute(ReviewFilter filter)
     {
-        throw new NotImplementedException();
+        return _db.Reviews.Select(r => _mapper.Map<ReviewEntity>(r)).Where(r => r.Stars == filter.Stars);
     }
 }
