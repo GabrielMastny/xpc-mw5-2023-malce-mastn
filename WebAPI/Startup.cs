a9074f2e660abb0debc01cdc700dcffe7f7aa6c0
Gabriel Mastny <gmastny@bjsw.cz>
2023-04-11 21:08:02 +0200

    removed unused namespace

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -5,7 +5,6 @@ using EFDb.Context;
 using EFDb.Mappings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
