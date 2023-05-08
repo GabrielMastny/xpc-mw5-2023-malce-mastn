d2cf25f4c1966f9e5ddaae400eaefbfd296c3b76
xmalce00 <60735480+xmalce00@users.noreply.github.com>
2023-05-08 15:45:30 +0200

    update queries

--- Eshop.DAL/QueryObjects/GetManufacturersByManufacturerDataFilterQuery.cs
+++ Eshop.DAL/QueryObjects/GetManufacturersByManufacturerDataFilterQuery.cs
@@ -20,8 +20,15 @@ public class GetManufacturersByManufacturerDataFilterQuery : IQuery<Manufacturer
     public IEnumerable<ManufacturerEntity> Execute(ManufacturerDataFilter filter)
     {
         IEnumerable<ManufacturerEntity> list = _db.Comodities.Select(m => _mapper.Map<ManufacturerEntity>(m));
-        if (filter.Name != null) list = list.Where(m => m.Name.Contains(filter.Name));
-        if (filter.CountryOfOrigin != null) list = list.Where(m => m.CountryOfOrigin == filter.CountryOfOrigin);
+        if (filter.Name != null)
+        {
+            list = list.Where(m => m.Name.Contains(filter.Name));
+        }
+
+        if (filter.CountryOfOrigin != null)
+        {
+            list = list.Where(m => m.CountryOfOrigin == filter.CountryOfOrigin);
+        }
         return list;
     }
 }
--- Eshop.DAL/QueryObjects/GetReviewsByReviewFilterQuery.cs
+++ Eshop.DAL/QueryObjects/GetReviewsByReviewFilterQuery.cs
@@ -1,13 +1,24 @@
-﻿using Eshop.DAL.Entities;
+﻿using AutoMapper;
+using Eshop.DAL.Entities;
 using CommonDbProperties.Interfaces.Filters;
 using CommonDbProperties.Interfaces.QueryObjects;
+using Eshop.DAL.Context;
 
 namespace Eshop.DAL.QueryObjects;
 
 public class GetReviewsByReviewFilterQuery : IQuery<ReviewEntity, ReviewFilter>
 {
+    
+    private readonly EshopContext _db;
+    private readonly IMapper _mapper;
+    
+    public GetReviewsByReviewFilterQuery(EshopContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
     public IEnumerable<ReviewEntity> Execute(ReviewFilter filter)
     {
-        throw new NotImplementedException();
+        return _db.Reviews.Select(c => _mapper.Map<ReviewEntity>(c)).Where(c => c.Stars == filter.Stars);
     }
 }
